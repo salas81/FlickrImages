ac9b812024189b519137762bc61c4d95b7ffe144
Lorenzo Decaria <lorenzo.decaria@gmail.com>
2025-01-16 16:06:45 +0100

    Initial Commit

---
+++ FlickrGallery/FlickrGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrGalleryApp.swift
+//  FlickrGallery
+//
+//  Created by Lorenzo on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct FlickrGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickrGalleryTests/FlickrGalleryTests.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrGalleryTests.swift
+//  FlickrGalleryTests
+//
+//  Created by Lorenzo on 1/16/25.
+//
+
+import Testing
+
+struct FlickrGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
